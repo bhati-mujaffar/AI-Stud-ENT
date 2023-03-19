375980a538aac02234a471c020785c3ad6475bf0
Bhati Mujaffar <mujaffarbhati222@gmail.com>
2023-03-19 16:10:36 +0530

    Added Scan image Section

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask
 from flask import render_template, request
 from flask import redirect, flash 
+import imghdr
 import json
 with open("config.json","r") as c:
     params=json.load(c)["params"]
 
 app = Flask(__name__)
 
-@app.route("/")
+# @app.route("/",methods=["POST"])
+# def textchat():
+#     
+#     return render_template('index.html',params=params)
+
+@app.route('/', methods=['GET', 'POST'])
 def textchat():
+    if request.method == 'POST':
+        img = request.files['images']
+        link = "temp."+imghdr.what(img)
+        img.save(link)
+        print(img)
+        print("lol")
     return render_template('index.html',params=params)
+
+
 
 @app.route("/dashboard")
 def dashboard():
